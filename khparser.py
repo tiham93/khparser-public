86624e4361d62f05d9688d7a1d6065e5a929bcd2
tiham93 <longphn93@gmail.com>
2025-04-04 11:59:55 +0700

    v0.5.0. Move to fzf for tUI. Use aria2 for download.

--- khparser.py
+++ khparser.py
@@ -3,100 +3,165 @@ import requests
 from bs4 import BeautifulSoup as bs
 # from subprocess import run
 from os.path import dirname, abspath, join#, exists
+from os import system
+from os import environ, startfile
+import sys
 from sys import argv
+from subprocess import run, PIPE, Popen
 from gallery_dl.job import DownloadJob as dl
 from gallery_dl import config
+from gallery_dl.job import DataJob as DJ
+
+def fzf_filter(input_func, fzf_options:str=''):
+	# set fzf option env var and spawn fzf process
+	env = environ.copy()
+	env['FZF_DEFAULT_OPTS'] = fzf_options
+	process = Popen('fzf --layout=reverse', text=True, env=env, stdin=PIPE, stdout=PIPE, encoding='utf-8')
+
+	# save original sys.stdout and redirect sys.stdout to fzf.stdin
+	original_stdout = sys.stdout
+	sys.stdout = process.stdin
+
+	input_func()	# print input_func results to stdout (currently fzf.stdin)
+
+	# pipe to fzf done: close fzf.stdin and return sys.stdout to original
+	process.stdin.close()
+	sys.stdout = original_stdout
+
+	# read fzf.stdout and return the results
+	results = process.stdout.read().splitlines()
+	process.stdout.close()
+	return results
+
+def change_format(current_format):
+	inp = fzf_filter(
+		lambda: print('\n'.join([
+			'1. mp3',
+			'2. flac (if exists, fallback to mp3 otherwise)'])),
+		fzf_options='--reverse --header="Select Format (current=%s)" --bind=\'esc:clear-query\'' % current_format)
+	if not len(inp):
+		return current_format
+	inp = inp[0][0:1]
+	match inp:
+		case '1':
+			download_format = 'mp3'
+			config.set(("extractor", "khinsider"), "format", "mp3")
+		case '2':
+			download_format = 'flac'
+			# gallery_dl_options = '--write-metadata -o format=flac -d ' + download_location
+			config.set(("extractor", "khinsider"), "format", "flac")
+	return download_format
+
+def dl_loc(name=''):
+	pwd = dirname(abspath(argv[0]))
+	download_location = join(pwd, 'downloads', 'khinsider', name)
+	return download_location
+
+def download(url):
+	print('\033[96m' + '\nDOWNLOADING ' + '\033[0m' + url)
+	tmp_file = join(environ['TMP'], 'khparser_tmp')
+	with open(tmp_file, 'w') as file:
+		DJ(url, file=file).run()
+		# print('urljob done')
+
+	urls = []
+	with open(tmp_file, 'r') as file:
+		from json import load
+		data = load(file)
+		for item in data:
+			if item[0] == 2:
+				name = item[1]['album']['name']
+				name = "".join(i for i in name if i not in "\\/:*?<>|")
+			elif item[0] == 3 and type(item[1]) == type(''):
+				urls.append(item[1])
+
+	with open(tmp_file, 'w') as file:
+		file.write('\n'.join(urls))
+
+	run(['aria2c', 
+		'--dir=%s' % dl_loc(name), 
+		'--max-concurrent-downloads=1', 
+		'--connect-timeout=60', 
+		'--max-connection-per-server=16',
+		'--split=16',
+		'--min-split-size=1M',
+		'--human-readable=true',
+		'--download-result=full',
+		'--file-allocation=none',
+		'--conditional-get',
+		'-i %s' % tmp_file])
+	system('del "%s"' % tmp_file)
+	startfile(dl_loc(name))
 
 def main():
 	config.load()
-	pwd = dirname(abspath(argv[0]))
 	# gallery_dl_path = join(pwd, 'gallery-dl.exe')
 	# print('gallery-dl path:', gallery_dl_path)
 	# if not exists(gallery_dl_path):
 	# 	inp = input('ERR: gallery-dl not found in current directory')
 	# 	return
 		
-	download_location = join(pwd, 'downloads')
 	download_format = 'flac'
 	# gallery_dl_options = '--write-metadata -o format=flac -d ' + download_location
 	config.set(("extractor", "khinsider"), "format", "flac")
-	config.set(("extractor", "khinsider"), "base-directory", download_location)
+	# config.set(("extractor", "khinsider"), "base-directory", download_location)
 
 	while True:
-		print('Select Mode')
-		print('1. Search Khinsider')
-		print('2. Paste URL(s)')
-		print('3. Change download format (current=' + download_format + ')')
-		mode = input('> ')
+		system('cls')
+		modes = fzf_filter(
+			lambda: print('\n'.join([
+				'1. Search Khinsider',
+				'2. Paste URL(s)',
+				'3. Change download format (current=%s)' % download_format])),
+			fzf_options='--reverse --header="Select Mode" --bind=\'esc:clear-query\'')
 
-		if mode.strip() == '3':
-			print('Select Format')
-			print('1. mp3')
-			print('2. flac (if exists, otherwise mp3)')
-			inp = input('> ')
-			if inp.strip() == '1':
-				download_format = 'mp3'
-				# gallery_dl_options = '--write-metadata -o format=mp3 -d ' + download_location
-				config.set(("extractor", "khinsider"), "format", "mp3")
-			elif inp.strip() == '2':
-				download_format = 'flac'
-				# gallery_dl_options = '--write-metadata -o format=flac -d ' + download_location
-				config.set(("extractor", "khinsider"), "format", "flac")
-			else:
-				print('Invalid input. Nothing changed.')
+		if len(modes)==0:
+			return
+		mode = modes[0][0:1]
 
-		if mode.strip() == '2':
-			inp = input('URL(s): ')
-			urls = inp.split()
-			for url in urls:
-				print('\nDownloading ' + url)
-				# run_command = ' '.join([gallery_dl_path, gallery_dl_options, url])
-				# run(run_command)
-				dl(url).run()
+		match mode:
+			case '3':
+				download_format = change_format(download_format)
+			case '2':
+				inp = input('URL(s): ')
+				inp = inp.strip()
+				if not inp:
+					continue
+				urls = inp.split()
+				for url in urls:
+					download(url)
+			case '1':
+				inp = input('Search khinsider: ')
+				inp = inp.strip()
+				if not inp:
+					continue
+				request = 'https://downloads.khinsider.com/search?search=%s&sort=timestamp' % inp
+				r = requests.get(request)
+				soup = bs(r.text,'html.parser')
+				links = {}
+				url_base = 'https://downloads.khinsider.com'
+				for link in soup.find_all('a'):
+					link_text = link.get('href')
+					if link_text and link_text.find('/album/') != -1:
+						if link.string:
+							links[link.string] = url_base + link_text
 
-		elif mode.strip() == '1':
-			p=input('Search khinsider: ')
-			request = 'https://downloads.khinsider.com/search?search=' + p + '&sort=timestamp'
-			r = requests.get(request)
-			soup = bs(r.text,'html.parser')
-			links = {}
-			for link in soup.find_all('a'):
-				link_text = link.get('href')
-				if link_text.find('/album/') != -1:
-					if link.string:
-						links[link.string] = link_text
-
-			idx = 0
-			urls = []
-			url_base = 'https://downloads.khinsider.com'
-			for name in links:
-				idx += 1
-				print(str(idx) + ') ' + name)
-				urls.append(url_base + links[name])
-
-			while True:
-				p=input('Download (space separated, 0=all, r=reset): ')
-				if p=='r':
-					break
-				if p=='0':
-					downloads=urls
-				else:
-					dl_ids = p.split()
-					downloads=[]
-					for id in dl_ids:
-						try:
-							if int(id)<=len(urls) and int(id)>0:
-								downloads.append(urls[int(id)-1])
-						except:
-							pass
-
-				for url in downloads:
-					print('\nDownloading ' + url)
-					# run_command = ' '.join([gallery_dl_path, gallery_dl_options, url])
-					# run(run_command)
-					dl(url).run()
+				selected = fzf_filter(
+						lambda: print('\n'.join([file for file in links])),
+						fzf_options='\
+							--reverse \
+							--header="Select album(s) to download:" \
+							--wrap \
+							--multi \
+							--bind=\'esc:clear-query,ctrl-a:select-all,ctrl-d:deselect-all\'')
+				if len(selected) == 0:
+					continue
+				for file in selected:
+					download(links[file])
 
 				p=input('\nDONE. Press any key...\n')
-		print('\n=========================')
 
-main()
+try:
+	main()
+except KeyboardInterrupt:
+	pass
